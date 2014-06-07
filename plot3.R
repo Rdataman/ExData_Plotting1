d09490308d513226b99c0be6efd523f2b6a1b87d
Rdataman <tclwhu@gmail.com>
2014-06-07 20:44:12 +0300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,24 @@
+collist1=list("","","","","","","","","")
+collist=list("","",0,0,0,0,0,0,0)
+cname<-scan("household_power_consumption.txt",what=collist1,nlines=1,sep=";")
+a<- scan("household_power_consumption.txt",what=collist,sep=";",skip=1,na.string= "?")
+## Read the colnames and data seperately
+
+data0<-data.frame(a)  
+## Change form of data from "list" to "dataframe"
+colnames(data0)<-cname
+data<-data0[which(data0$Date=="1/2/2007"|data0$Date=="2/2/2007"),1:9]
+## Select the records on 1/2/2007 and 2/2/2007
+data$DayTime<- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+## Add one col "DayTime" show the real time 
+
+## Plot3
+Sys.setlocale(locale = "C")
+with(data,plot(DayTime,Sub_metering_1,type="l",xlab=NA,ylab="Energy sub metering"))
+with(data,lines(DayTime,Sub_metering_2,type="l",col="red"))
+with(data,lines(DayTime,Sub_metering_3,type="l",col="blue"))
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black","red","blue"),lty = c(1, 1, 1))
+# save pic
+dev.copy(png, file = "plot3.png",width = 480, height = 480, units = "px")
+dev.off()
